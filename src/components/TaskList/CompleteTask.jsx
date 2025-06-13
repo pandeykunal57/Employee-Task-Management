8c9ed3c856f81c8625f18e245dd5632d488595be
pandeykunal57 <148645697+pandeykunal57@users.noreply.github.com>
2025-06-13 14:30:38 +0530

    TaskList Mapped

---
+++ src/components/TaskList/CompleteTask.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const CompleteTask = () => {
+  return (
+    <div className='flex-shrink-0 h-full w-[300px] bg-cyan-400 rounded-xl p-5'>
+        <div className='flex justify-between  items-center'>
+            <h3 className='bg-green-600 px-3 text-xs py-1 rounded-md font-bold'>low</h3>
+            <h4 className='font-bold text-sm'>9 June 2025</h4>
+        </div>
+        <h2 className='text-3xl mt-4 font-semibold'>TASK 1</h2>
+        <p className='mt-2 '>kjasdgcfku ajsckua kjasdcfk kjadsc ladjc dgu kadjfbakdfgk</p>
+        <div className='mt-10 '>
+            <button className='bg-green-500 px-3 py-2 rounded text-xs font-medium'>COMPLETED</button>
+        </div>
+        </div>
+  )
+}
+
+export default CompleteTask
--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,52 +1,43 @@
 import React from 'react'
+import AcceptTask from './AcceptTask'
+import NewTask from './NewTask'
+import FailedTask from './FailedTask'
+import CompleteTask from './CompleteTask'
 
 const TaskList = ({data}) => {
   return (
     <div id='tasklist' className='h-[55%] overflow-x-auto w-full flex flex-nowrap items-center justify-start gap-5 py-5 mt-10'>
-        <div className='flex-shrink-0 h-full w-[300px] bg-gray-400 rounded-xl p-5'>
-        <div className='flex justify-between  items-center'>
-            <h3 className='bg-red-600 px-3 text-xs py-1 rounded-md font-bold'>High</h3>
-            <h4 className='font-bold text-sm'>9 June 2025</h4>
-        </div>
-        <h2 className='text-3xl mt-4 font-semibold'>TASK 1</h2>
-        <p className='mt-2 '>kjasdgcfku ajsckua kjasdcfk kjadsc ladjc dgu kadjfbakdfgk</p>
-        </div>
+        
+        {data.tasks.map((elem)=>{
+            if(elem.active){
+                return <AcceptTask/>
 
-        <div className='flex-shrink-0 h-full w-[300px] bg-cyan-400 rounded-xl p-5'>
-        <div className='flex justify-between  items-center'>
-            <h3 className='bg-green-600 px-3 text-xs py-1 rounded-md font-bold'>low</h3>
-            <h4 className='font-bold text-sm'>9 June 2025</h4>
-        </div>
-        <h2 className='text-3xl mt-4 font-semibold'>TASK 1</h2>
-        <p className='mt-2 '>kjasdgcfku ajsckua kjasdcfk kjadsc ladjc dgu kadjfbakdfgk</p>
-        </div>
+            }
 
-        <div className='flex-shrink-0 h-full w-[300px] bg-lime-400 rounded-xl p-5'>
-        <div className='flex justify-between  items-center'>
-            <h3 className='bg-yellow-500 px-3 text-xs py-1 rounded-md font-bold'>Med</h3>
-            <h4 className='font-bold text-sm'>9 June 2025</h4>
-        </div>
-        <h2 className='text-3xl mt-4 font-semibold'>TASK 1</h2>
-        <p className='mt-2 '>kjasdgcfku ajsckua kjasdcfk kjadsc ladjc dgu kadjfbakdfgk</p>
-        </div>
+            if(elem.newTask)
+            {
+                return <NewTask/>
+            }
+             if(elem.completed){
+                return <CompleteTask/>
 
-        <div className='flex-shrink-0 h-full w-[300px] bg-purple-400 rounded-xl p-5'>
-        <div className='flex justify-between  items-center'>
-            <h3 className='bg-red-600 px-3 text-xs py-1 rounded-md font-bold'>High</h3>
-            <h4 className='font-bold text-sm'>9 June 2025</h4>
-        </div>
-        <h2 className='text-3xl mt-4 font-semibold'>TASK 1</h2>
-        <p className='mt-2 '>kjasdgcfku ajsckua kjasdcfk kjadsc ladjc dgu kadjfbakdfgk</p>
-        </div>
+            }
 
-        <div className='flex-shrink-0 h-full w-[300px] bg-pink-400 rounded-xl p-5'>
-        <div className='flex justify-between  items-center'>
-            <h3 className='bg-red-600 px-3 text-xs py-1 rounded-md font-bold'>High</h3>
-            <h4 className='font-bold text-sm'>9 June 2025</h4>
-        </div>
-        <h2 className='text-3xl mt-4 font-semibold'>TASK 1</h2>
-        <p className='mt-2 '>kjasdgcfku ajsckua kjasdcfk kjadsc ladjc dgu kadjfbakdfgk</p>
-        </div>
+            if(elem.failed)
+            {
+                return <FailedTask/>
+            }
+        })}
+        
+        <AcceptTask/>
+        <NewTask/>
+        <CompleteTask/>
+        <FailedTask/>
+
+
+       
+
+       
       
     </div>
   )
